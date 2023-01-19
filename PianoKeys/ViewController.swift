a789bebf32429fd51134f3bcf4e87866e798cb34
Tim Akhm <akhmtimyr@gmail.com>
2023-01-19 18:17:26 +0200

    Initial Commit

---
+++ PianoKeys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PianoKeys
+//
+//  Created by Tim Akhmetov on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
